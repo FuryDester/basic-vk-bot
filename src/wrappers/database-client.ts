a715150396a3664a99ba9eb1969b408cbc824c07
Motin Mikhail Sergeevich <m.motin@qsoft.ru>
2022-11-27 18:08:35 +0300

    Added database client

---
+++ src/wrappers/database-client.ts
@@ -0,0 +1,48 @@
+import LokiDatabase from 'lokijs';
+
+class DatabaseClient {
+  private db: LokiDatabase;
+
+  private isAvailable: boolean;
+
+  constructor(databaseName: string, onAvailability?: () => void) {
+    this.isAvailable = false;
+
+    this.db = new LokiDatabase(`${databaseName}.db`, {
+      autoload         : true,
+      autosave         : true,
+      autosaveInterval : 2000,
+      autoloadCallback : () => {
+        this.isAvailable = true;
+        if (onAvailability) {
+          onAvailability();
+        }
+      },
+    });
+  }
+
+  afterAvailability(callback: () => void, hold: number = 500): void {
+    let interval = setInterval(() => {
+      if (this.isAvailable) {
+        clearInterval(interval);
+        callback();
+      }
+    }, hold > 0 ? hold : 500);
+  }
+
+  getOrAddCollection(name: string, options?: Partial<CollectionOptions<unknown>> | undefined): Collection<object> {
+    return this.db.getCollection(name) || this.db.addCollection(name, options);
+  }
+
+  getDatabase(): LokiDatabase {
+    return this.db;
+  }
+
+  setDatabase(db: LokiDatabase): DatabaseClient {
+    this.db = db;
+
+    return this;
+  }
+}
+
+export const databaseClient = new DatabaseClient(process.env.DATABASE_NAME!);
