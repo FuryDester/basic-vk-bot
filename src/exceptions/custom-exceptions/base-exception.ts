efd1605fdeff83271da17618613f77d174ffe5a2
Motin Mikhail Sergeevich <m.motin@qsoft.ru>
2022-11-30 10:37:02 +0300

    Added options for BaseException

--- src/exceptions/custom-exceptions/base-exception.ts
+++ src/exceptions/custom-exceptions/base-exception.ts
@@ -1,12 +1,26 @@
 abstract class BaseException extends Error {
-  constructor(msg?: string) {
+  private options: Record<string, unknown>;
+
+  constructor(msg?: string, options?: Record<string, unknown>) {
     super(msg);
+
+    this.options = options;
 
     Object.setPrototypeOf(this, new.target.prototype);
     this.name = this.getName();
   }
 
   abstract getName(): string;
+
+  getOptions(): Record<string, unknown> {
+    return this.options;
+  }
+
+  setOptions(options: Record<string, unknown>): BaseException {
+    this.options = options;
+
+    return this;
+  }
 }
 
 export default BaseException;
