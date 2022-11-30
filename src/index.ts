7ad1c634f1029b8254fa1ffaea0d2bc1973680b9
Motin Mikhail Sergeevich <m.motin@qsoft.ru>
2022-11-30 13:23:30 +0300

    On process exit event

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,10 @@ import { resolve } from 'path';
 config({ path: resolve(__dirname, '../.env') });
 
 import '@/exceptions/handler';
+
+// TODO: Make event handler?
+import { databaseClient } from '@/wrappers/database-client';
+
+process.on('exit', () => {
+  databaseClient.close();
+});
