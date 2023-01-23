a94460275c1cbf70db34eec53386c83e98d5bc1d
Motin Mikhail Sergeevich <m.motin@qsoft.ru>
2023-01-24 00:53:52 +0300

    Added listener logic

--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,22 @@ import '@/exceptions/handler';
 import { databaseClient } from '@/wrappers/database-client';
 import GroupsData from '@/models/groups-data';
 import Logger from '@/wrappers/logger';
-import { LogChannelEnum } from '@/enums';
 import GroupDto from '@/data-transfer-objects/models/group-dto';
 import { VkClient } from '@/wrappers/vk-client';
 import VkBotPollingException from '@/exceptions/custom-exceptions/vk-bot-polling-exception';
+import Listeners from '@/events/listeners';
 
-Logger.info('Starting up...', LogChannelEnum.Console);
+Logger.info('Starting up...');
 
 const groupsData = (new GroupsData()).getTable();
 let clients = [];
 groupsData.find().forEach((group: GroupDto & object & LokiObj) => {
-  Logger.info(`Group: ${group.id} (${group.name}) found`, LogChannelEnum.Console);
+  Logger.info(`Group: ${group.id} (${group.name}) found`);
 
   const client = new VkClient(group.id, group.token, false);
+  Object.keys(Listeners).forEach((listenerName: string) => {
+    client.event(listenerName, Listeners[listenerName].handle);
+  });
 
   client.startPolling((err: any) => {
     if (err) {
@@ -43,4 +46,6 @@ process.on('exit', () => {
   clients.forEach((client: VkClient) => {
     client.stop();
   });
+
+  Logger.info('Stopping...');
 });
