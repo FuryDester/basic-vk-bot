b0a1110859a34b06ed4f7245c4aef4af1afd5435
Motin Mikhail Sergeevich <m.motin@qsoft.ru>
2023-01-23 21:31:58 +0300

    Added polling method

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,37 @@ import '@/exceptions/handler';
 
 // TODO: Make event handler?
 import { databaseClient } from '@/wrappers/database-client';
+import GroupsData from '@/models/groups-data';
+import Logger from '@/wrappers/logger';
+import { LogChannelEnum } from '@/enums';
+import GroupDto from '@/data-transfer-objects/group-dto';
+import { VkClient } from '@/wrappers/vk-client';
+import VkBotPollingException from '@/exceptions/custom-exceptions/vk-bot-polling-exception';
+
+Logger.info('Starting up...', LogChannelEnum.Console);
+
+const groupsData = (new GroupsData()).getTable();
+let clients = [];
+groupsData.find().forEach((group: GroupDto & object & LokiObj) => {
+  Logger.info(`Group: ${group.id} (${group.name}) found`, LogChannelEnum.Console);
+
+  const client = new VkClient(group.id, group.token, false);
+
+  client.startPolling((err: any) => {
+    if (err) {
+      throw new VkBotPollingException('Error while startPolling on start script', {
+        id   : group.id,
+        data : err,
+      });
+    }
+  });
+  clients.push(client);
+});
 
 process.on('exit', () => {
   databaseClient.close();
+
+  clients.forEach((client: VkClient) => {
+    client.stop();
+  });
 });
--- src/wrappers/vk-client.ts
+++ src/wrappers/vk-client.ts
@@ -5,12 +5,12 @@ export class VkClient {
 
   private groupId: number;
 
-  constructor(groupId: number, token: string, startPolling: boolean = true) {
+  constructor(groupId: number, token: string, startPolling: boolean = true, onError?: (err: any) => {}) {
     this.groupId = groupId;
     this.bot = new VkBot(token);
 
     if (startPolling) {
-      this.bot.startPolling();
+      this.bot.startPolling(onError);
     }
   }
 
@@ -24,12 +24,30 @@ export class VkClient {
     return this;
   }
 
+  startPolling(onError?: (err: any) => void): VkClient {
+    this.getBotInstance().startPolling(onError);
+
+    return this;
+  }
+
   getGroupId(): number {
     return this.groupId;
   }
 
   setGroupId(groupId: number): VkClient {
     this.groupId = groupId;
+
+    return this;
+  }
+
+  stop(): VkClient {
+    this.getBotInstance().stop();
+
+    return this;
+  }
+
+  on(...middlewares: VkBotMiddleware[]): VkClient {
+    this.getBotInstance().on(...middlewares);
 
     return this;
   }
