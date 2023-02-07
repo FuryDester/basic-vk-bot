7cc496684204d0f30e76c8d3d481a7f67913f5cd
Motin Mikhail Sergeevich <m.motin@qsoft.ru>
2023-02-07 18:33:29 +0300

    Fixed hasTechStatisticsAbility function

--- src/logic/helpers/misc/has-tech-statistics-ability.ts
+++ src/logic/helpers/misc/has-tech-statistics-ability.ts
@@ -3,9 +3,11 @@ import GroupMemberDto from '@/data-transfer-objects/models/group-member-dto';
 import { GroupMemberPermissionEnum, GroupPermissionEnum } from '@/enums';
 
 export default (group: GroupDto, user: GroupMemberDto): boolean => {
-  return group.permissions.includes(GroupPermissionEnum.TechStatistics)
-    && (
-      user.permissions.includes(GroupMemberPermissionEnum.CountTechStatistics)
+  return (
+    group.permissions.includes(GroupPermissionEnum.TechStatistics)
+    || group.permissions.includes(GroupPermissionEnum.All)
+  ) && (
+    user.permissions.includes(GroupMemberPermissionEnum.CountTechStatistics)
       || user.permissions.includes(GroupMemberPermissionEnum.All)
-    );
+  );
 };
